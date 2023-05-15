131eed1661880e309e16022602bd5b88311b7f3c
Mahmudul Hasan Zunnun <33092475+znun@users.noreply.github.com>
2023-05-15 17:33:08 +0600

    new change

---
+++ PopularMovies/Model.swift
@@ -0,0 +1,31 @@
+//
+//  Model.swift
+//  PopularMovies
+//
+//  Created by Mahmudul Hasan on 15/5/23.
+//
+
+import Foundation
+
+struct moviesData: Decodable {
+    let movies: [Movie]
+    
+    private enum CodingKeys: String, CodingKey {
+        case movies = "results"
+    }
+}
+
+struct Movie: Decodable {
+    let title: String?
+    let year: String?
+    let rating: Double?
+    let posterImage: String?
+    let overview: String?
+   
+    private enum CodingKeys: String, CodingKey {
+        case title, overview
+        case year = "release_date"
+        case rating = "vote_average"
+        case posterImage = "poster_path"
+    }
+}
